9f691adf0e5566c8665701d405e6235a33c652e4
Budova Diana <dianachampions011@gmail.com>
2024-05-31 21:56:25 +0300

    Add miniatures. Change logo. Change theme

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -66,18 +66,6 @@ class Header extends \Opencart\System\Engine\Controller {
 
 		$data['home'] = $this->url->link('common/home', 'language=' . $this->config->get('config_language'));
 		$data['wishlist'] = $this->url->link('account/wishlist', 'language=' . $this->config->get('config_language') . (isset($this->session->data['customer_token']) ? '&customer_token=' . $this->session->data['customer_token'] : ''));
-		$data['logged'] = $this->customer->isLogged();
-
-		if (!$this->customer->isLogged()) {
-			$data['register'] = $this->url->link('account/register', 'language=' . $this->config->get('config_language'));
-			$data['login'] = $this->url->link('account/login', 'language=' . $this->config->get('config_language'));
-		} else {
-			$data['account'] = $this->url->link('account/account', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']);
-			$data['order'] = $this->url->link('account/order', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']);
-			$data['transaction'] = $this->url->link('account/transaction', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']);
-			$data['download'] = $this->url->link('account/download', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']);
-			$data['logout'] = $this->url->link('account/logout', 'language=' . $this->config->get('config_language'));
-		}
 
 		$data['shopping_cart'] = $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'));
 		$data['checkout'] = $this->url->link('checkout/checkout', 'language=' . $this->config->get('config_language'));
